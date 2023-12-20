9e45559eabef9e16e861db56bcdf3725834e0f87
Stefan Danila <100301698+stefanDanila0@users.noreply.github.com>
2023-12-20 21:46:57 +0200

    Final Prophet

--- prophet_analysis.py
+++ prophet_analysis.py
@@ -21,7 +21,7 @@ def predict(start=0, end=168, week=0, echo=True):
         week: int, optional
             Train on the data from the specified week (default is 0). If provided, it ignores start and end parameters
         echo: bool, optional
-            Enables or disables prediction outputs (default is True)
+            Enables or disables prediction outputs, as well as display of predicted_vs_actual plots (default is True)
         Returns
         -------
         list
@@ -33,10 +33,18 @@ def predict(start=0, end=168, week=0, echo=True):
     else:
         if week != 0:
             start = (week - 1) * 168
-            end = start + 169
+            end = (start + 169) % 1001
     models = []  # for each column, train the model on that variable and add it to the list
+    if not os.path.exists("predicted_vs_actual_plots"):
+        os.makedirs("predicted_vs_actual_plots")
+
     for label in labels[1:]:
         df_temp = df[[labels[0], label]]
+
+        if end + 48 > 1000:
+            end -= 48
+        actual_df = df_temp[start:(end + 48)]
+        actual_df.columns = ['ds', 'y']
 
         ts = df_temp[start:end]
         ts.columns = ['ds', 'y']
@@ -48,22 +56,23 @@ def predict(start=0, end=168, week=0, echo=True):
         forecast = model.predict(future)
 
         file_path = f"predicted_vs_actual_plots/predicted_vs_actual_{label}_values.png"
+        forecast.rename(columns={"ds": "Timestamp", "yhat": "Predicted Value"}, inplace=True)
 
         if echo:
-            forecast.rename(columns={"ds": "Timestamp", "yhat": "Predicted Value"}, inplace=True)
             print(f"----------- {label} ------------")
             print(forecast[['Timestamp', 'Predicted Value']].tail(48))
-            print(f"forecast HEAD: {forecast.head()}")
-            plt.figure(figsize=(10, 6))
-            plt.plot(ts['ds'], ts['y'], label='Actual')
-            plt.plot(forecast['Timestamp'], forecast['Predicted Value'], label='Predicted')
-            plt.xlabel('Date')
-            plt.ylabel('Value')
-            plt.title(f'Actual vs Predicted - {label}')
-            plt.legend()
-            plt.savefig(file_path)
+
+        plt.figure(figsize=(10, 6))
+        plt.plot(actual_df['ds'], actual_df['y'], label='Actual')
+        plt.plot(forecast['Timestamp'], forecast['Predicted Value'], label='Predicted')
+        plt.xlabel('Date')
+        plt.ylabel('Value')
+        plt.title(f'Actual vs Predicted - {label}')
+        plt.legend()
+        plt.savefig(file_path)
+        if echo:
             plt.show()
-            plt.close()
+        plt.close('all')
 
         models += [model]
 
@@ -85,31 +94,19 @@ def get_cross_validation(echo=True):
         df_cv += [
             cross_validation(model, initial='168 hours', period='168 hours', horizon='48 hours')]
     i = 1
+    if not os.path.exists("cross_validation_values"):
+        os.makedirs("cross_validation_values")
     for df_cross_err in df_cv:
         df_p = performance_metrics(df_cross_err)
         if echo:
             print(f"---------{labels[i]}---------\n", df_p.tail(1))
         try:
-            plot_cross_validation_metric(pd.DataFrame(df_cross_err), metric='mape')
-            plt.title(f'MAPE - {str(labels[i])}')
-            plt.xlabel('Horizon')
-            plt.ylabel('MAPE')
-            folder_path = 'cross_validation_values'
-            if not os.path.exists(folder_path):
-                os.makedirs(folder_path)
-            plt.savefig(os.path.join(folder_path, f"MAPE_{str(labels[i])}.png"))
-            plt.close()
+            plot_cross_validation_metric(pd.DataFrame(df_cross_err), metric='mape').savefig(
+                f"cross_validation_values/MAPE - {str(labels[i])}.png")
             i += 1
         except TypeError:
-            plot_cross_validation_metric(pd.DataFrame(df_cross_err), metric='mae')
-            plt.title(f'MAE - {str(labels[i])}')
-            plt.xlabel('Horizon')
-            plt.ylabel('MAE')
-            folder_path = 'cross_validation_values'
-            if not os.path.exists(folder_path):
-                os.makedirs(folder_path)
-            plt.savefig(os.path.join(folder_path, f"MAE_{str(labels[i])}.png"))
-            plt.close()
+            plot_cross_validation_metric(pd.DataFrame(df_cross_err), metric='mae').savefig(
+                f"cross_validation_values/MAE - {str(labels[i])}.png")
             i += 1
 
 
