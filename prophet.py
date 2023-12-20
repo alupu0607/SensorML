2755de043313ab7c67299cd892711b6a14e56a8c
Stefan Danila <100301698+stefanDanila0@users.noreply.github.com>
2023-12-20 17:30:39 +0200

    Update prophet.py
    Added docstrings, possibility to disable prints and the week parameter

--- prophet.py
+++ prophet.py
@@ -3,17 +3,39 @@ from prophet import Prophet
 from prophet.diagnostics import cross_validation, performance_metrics
 from prophet.plot import plot_cross_validation_metric
 
-df = pd.read_csv('X:\\SensorMLDataset_small.csv')
+df = pd.read_csv('X:\\SensorMLDataset_small.csv')  # input the path to the CSV file here
 labels = df.columns
 
 
-def predict(start, end):
+def predict(start=0, end=168, week=0, echo=True):
+    """Trains a Prophet model on the variables from the provided CSV file
+        Parameters
+        ----------
+        start : int, optional
+            The start row from the CSV file from which the training begins (default is 0)
+        end : int, optional
+            The end row from the CSV file up to which the model trains (default is 168, as the current CSV
+            file has hourly data, meaning first week)
+        week: int, optional
+            Train on the data from the specified week (default is 0). If provided, it ignores start and end parameters
+        echo: bool, optional
+            Enables or disables prediction outputs (default is True)
+        Returns
+        -------
+        list
+            a list of the models trained on the variables from the CSV file
+        """
     df[labels[0]] = pd.to_datetime(df[labels[0]], format='%m/%d/%Y %H:%M')
-
-    models = []  # pentru fiecare parametru, adaugam modelul antrenat intr-o lista
+    if week - 1 > df.__len__() // 168:
+        print("Not enough weeks in the dataset. Defaulting to start, end parameters.")
+    else:
+        if week != 0:
+            start = (week - 1) * 168
+            end = start + 169
+    models = []  # for each column, train the model on that variable and add it to the list
     for label in labels[1:]:
         df_temp = df[[labels[0], label]]
-        print(f"----------- {label} ------------")
+
         ts = df_temp[start:end]
         ts.columns = ['ds', 'y']
 
@@ -22,15 +44,27 @@ def predict(start, end):
 
         future = model.make_future_dataframe(periods=48, freq='H')
         forecast = model.predict(future)
-        print(forecast[['ds', 'yhat']].tail(48))
+
+        if echo:
+            forecast.rename(columns={"ds": "Timestamp", "yhat": "Predicted Value"}, inplace=True)
+            print(f"----------- {label} ------------")
+            print(forecast[['Timestamp', 'Predicted Value']].tail(48))
 
         models += [model]
 
-    return models  # returnam lista cu modelele antrenate
+    return models  # return trained models
 
 
-def get_cross_validation():
-    models = predict(0, 1000)
+def get_cross_validation(echo=True):
+    """Computes the error at training using time-series cross validation on the entire dataset.
+    It outputs the calculated error plots for each variable as a .PNG file
+    Parameters
+    ----------
+        echo: bool, optional
+            Enables or disables computed errors outputs (default is True)
+    """
+    models = predict(0, df.__len__(), echo=False)
+    print("----- Computing time series cross validation -----")
     df_cv = []
     for model in models:
         df_cv += [
@@ -38,7 +72,8 @@ def get_cross_validation():
     i = 1
     for df_cross_err in df_cv:
         df_p = performance_metrics(df_cross_err)
-        print(f"---------{labels[i]}---------\n", df_p.tail(1))
+        if echo:
+            print(f"---------{labels[i]}---------\n", df_p.tail(1))
         try:
             plot_cross_validation_metric(pd.DataFrame(df_cross_err), metric='mape').savefig(
                 f"MAPE - {str(labels[i])}.png")
@@ -50,6 +85,7 @@ def get_cross_validation():
 
 
 def main():
+    predict(week=3)
     get_cross_validation()
 
 
