5a026f933ff4c69cd6f3c863fc92b7240d49cc32
andreea <dandreealupu@yahoo.com>
2024-01-21 20:20:23 +0200

    outlier removal + denormalized diagrams for lstm

--- lstm.py
+++ lstm.py
@@ -5,10 +5,12 @@ import pandas as pd
 import tensorflow as tf
 from sklearn.metrics import mean_absolute_error
 import os
+
+from tema1 import preprocess_data_IQR
+from tema1 import load_data
 def split_data(file_path):
     # The data is not randomly shuffled:
     # It ensures that chopping the data into windows of consecutive samples is still possible.
-
     df = pd.read_csv(file_path)
     date_time = pd.to_datetime(df.pop('Timestamp'),format='%m/%d/%Y %H:%M')
     print(df.shape[1]) # number of features (Timestamp is not a fetaure)
@@ -83,7 +85,7 @@ def make_dataset(self, data):
       targets=None,
       sequence_length=self.total_window_size,
       sequence_stride=1,
-      shuffle=True,
+      shuffle=False,
       batch_size=32,
       )
 
@@ -112,6 +114,17 @@ def example(self):
         self._example = result
     return result
 
+
+@property
+def test_last_predictions(self):
+    """Get and cache an example batch of `inputs, labels` for plotting."""
+    result = getattr(self, '_test_last_predictions', None)
+    if result is None:
+        result = next(iter(self.test))
+        self._test_last_predictions = result
+    return result
+
+
 def compile_and_fit(model, window, patience=2):
   #early stopping prevents overfitting
   early_stopping = tf.keras.callbacks.EarlyStopping(monitor='val_loss',
@@ -128,6 +141,64 @@ def compile_and_fit(model, window, patience=2):
 
   mae_values = history.history['mean_absolute_error']
   return history, mae_values
+
+
+def plot_future_test_set(self, model, plot_col, train_mean, train_std, save_folder='test_set_predictions_lstm'):
+    inputs, labels = self.test_last_predictions
+
+    plt.figure(figsize=(12, 8))
+    plot_col_index = self.column_indices[plot_col]
+
+    if self.label_columns:
+        label_col_index = self.label_columns_indices.get(plot_col, None)
+    else:
+        label_col_index = plot_col_index
+
+    if label_col_index is None:
+        return
+
+    plt.ylabel(f'{plot_col} [original scale]')
+
+    # De-normalize the inputs, labels, and predictions
+    last_24_hours_inputs_denormalized = inputs * train_std[plot_col] + train_mean[plot_col]
+    labels_denormalized = labels * train_std[plot_col] + train_mean[plot_col]
+
+    plt.plot(self.input_indices, last_24_hours_inputs_denormalized[-1, :, plot_col_index],
+             label='Inputs', marker='.', zorder=-10)
+
+    plt.scatter(self.label_indices, labels_denormalized[-1, :, label_col_index],
+                edgecolors='k', label='Actual Values', c='#2ca02c', s=64)
+
+    if model is not None:
+        last_24_hours_predictions = model(inputs)
+        last_24_hours_predictions_denormalized = last_24_hours_predictions * train_std[plot_col] + train_mean[plot_col]
+
+        plt.scatter(self.label_indices, last_24_hours_predictions_denormalized[-1, :, label_col_index],
+                    marker='X', edgecolors='k', label='Predictions (Last 24 Hours)',
+                    c='#ff7f0e', s=64)
+
+    plt.legend()
+    plt.xlabel('Timestamp')
+    save_path = os.path.join(save_folder, f'{plot_col}.png')
+    plt.savefig(save_path)
+    plt.close()
+
+
+def plot_future_test_set2(self, model, plot_col, train_mean, train_std, save_folder='test_set_predictions_lstm'):
+    inputs, labels = self.test_last_predictions
+
+    print(self.test_last_predictions)
+
+    predictions = model(inputs)
+
+    inputs = inputs * train_std[plot_col] + train_mean[plot_col]
+    labels= labels * train_std[plot_col] + train_mean[plot_col]
+    predictions = predictions * train_std[plot_col] + train_mean[plot_col]
+
+    print(labels)
+    #print(predictions)
+
+
 
 def plot_future_train_set(self, model, plot_col, max_subplots=3, save_folder='train_set_predictions_lstm'):
     inputs, labels = self.example
@@ -149,14 +220,14 @@ def plot_future_train_set(self, model, plot_col, max_subplots=3, save_folder='tr
         if label_col_index is None:
             continue
 
-        labels_slice = slice(self.label_start, self.label_start + self.label_width + 24)
+        #labels_slice = slice(self.label_start, self.label_start + self.label_width + 24)
         plt.scatter(self.label_indices, labels[n, :, label_col_index],
                     edgecolors='k', label='Labels', c='#2ca02c', s=64)
 
+
         if model is not None:
             predictions = model(inputs)
-            # Plot predictions for the future 24 hours
-            plt.scatter(self.label_indices[-24:], predictions[n, -24:, label_col_index],
+            plt.scatter(self.label_indices, predictions[n, :, label_col_index],
                         marker='X', edgecolors='k', label='Predictions (Future)',
                         c='#ff7f0e', s=64)
 
@@ -164,7 +235,7 @@ def plot_future_train_set(self, model, plot_col, max_subplots=3, save_folder='tr
             plt.legend()
 
     plt.xlabel('Timestamp')
-    save_path = os.path.join(save_folder, f'figure_{plot_col}.png')
+    save_path = os.path.join(save_folder, f'{plot_col}.png')
     plt.savefig(save_path)
     plt.close()
 def calculate_mae(actual_values, predicted_values):
@@ -188,8 +259,9 @@ WindowGenerator.train = train
 WindowGenerator.val = val
 WindowGenerator.test = test
 WindowGenerator.example = example
+WindowGenerator.test_last_predictions = test_last_predictions
 WindowGenerator.plot_future_train_set = plot_future_train_set
-
+WindowGenerator.plot_future_test_set = plot_future_test_set
 if __name__ == '__main__':
 
     file_path = "./SensorMLDataset.csv"
@@ -202,13 +274,15 @@ if __name__ == '__main__':
     val_df = (val_df - train_mean) / train_std
     test_df = (test_df - train_mean) / train_std
 
-    df = pd.read_csv(file_path)
+    ## OUTLIER REMOVAL
+    data = load_data(file_path)
+    df = preprocess_data_IQR(data)
 
     ### Save the average MAE for every param
     average_mae_per_param = []
 
     for param in df.columns:
-        if param != 'Timestamp':
+        if param != 'Timestamp' and param == 'pres':
 
             wide_window = WindowGenerator(
                 #the model takes 24 consecutive observations as input
@@ -217,7 +291,7 @@ if __name__ == '__main__':
                 input_width=24, label_width=24, shift=1, train_df= train_df,
                 val_df=val_df, test_df=test_df,
                 label_columns=[param])
-            print(wide_window)
+            print(train_df.shape, val_df.shape, test_df.shape )
 
 
             lstm_model = tf.keras.models.Sequential([
@@ -234,54 +308,11 @@ if __name__ == '__main__':
             print("Average Mean Absolute Error:", average_mae)
             average_mae_per_param.append(average_mae)
 
-            val_performance = {}
-            performance = {}
-            IPython.display.clear_output()
-            val_performance['LSTM'] = lstm_model.evaluate(wide_window.val)
-            performance['LSTM'] = lstm_model.evaluate(wide_window.test, verbose=0)
-
-
-
-            ### TEST ACCURACY OF OBTAINED DATA - TO DO ###
-            train_predictions = lstm_model.predict(wide_window.train)
-            train_predictions = train_predictions * train_std[param] + train_mean[param]
-
-            val_predictions = lstm_model.predict(wide_window.val)
-            val_predictions = val_predictions * train_std[param] + train_mean[param]
-
-            test_predictions = lstm_model.predict(wide_window.test)
-            test_predictions = test_predictions * train_std[param] + train_mean[param]
-
-
-            # De-normalize actual values - so as to show the last graphic with the next 1 hour prediction
-            train_actual_values = wide_window.train_df[param].values * train_std[param] + train_mean[param]
-            train_actual_values = train_actual_values[:-24]
-            val_actual_values = wide_window.val_df[param].values * train_std[param] + train_mean[param]
-            val_actual_values = val_actual_values[:-24]
-            test_actual_values = wide_window.test_df[param].values * train_std[param] + train_mean[param]
-            test_actual_values = test_actual_values[:-24]
-
-            sets = {
-                'Test': {'actual_values': test_actual_values, 'predicted_values': test_predictions},
-                'Validation': {'actual_values': val_actual_values, 'predicted_values': val_predictions},
-                'Eval': {'actual_values': test_actual_values, 'predicted_values': test_predictions}
-            }
-
-            for set_name, set_data in sets.items():
-                actual_values = set_data['actual_values']
-                predicted_values = set_data['predicted_values']
-
-
-                #actual_vs_predicted = create_actual_vs_predicted_list(actual_values, predicted_values)
-
-                #print(f"Actual versus Predicted Values for {set_name} Set:")
-                #for actual, predicted in actual_vs_predicted:
-                #    print(f"Actual: {actual}, Predicted: {predicted}")
 
             wide_window.plot_future_train_set(lstm_model,param)
+            wide_window.plot_future_test_set(lstm_model,param,train_mean, train_std)
 
 
-            #### PRINT THE LAST HOUR PREDICTED INTO THE FUTURE (SCALED)
 
 
 
--- tema1.py
+++ tema1.py
@@ -2,16 +2,30 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 import os
+import numpy as np
+
+
 
 def load_data(file_path):
     data = pd.read_csv(file_path, parse_dates=['Timestamp'])
     return data
 
+
 def preprocess_data(df):
     # Drop the hours from the Timestamp column
     df['Timestamp'] = df['Timestamp'].dt.date
-
     return df
+
+def preprocess_data_IQR(df):
+    # Drop the hours from the Timestamp column
+    df['Timestamp'] = df['Timestamp'].dt.date
+
+    Q1 = df.quantile(0.25)
+    Q3 = df.quantile(0.75)
+    IQR = Q3 - Q1
+    df = df[~((df < (Q1 - 1.5 * IQR)) | (df > (Q3 + 1.5 * IQR))).any(axis=1)]
+    return df
+
 
 def calculate_heatmaps(df):
     means = df.groupby('Timestamp').mean()
@@ -35,51 +49,90 @@ def calculate_heatmaps(df):
             plt.savefig(os.path.join("./median_values", f'median_heatmap_{param}.png'))
             plt.close()
 
-def calculate_boxplots(df):
+
+def feature_selection(df, correlation_threshold=0.8):
+    numeric_columns = df.select_dtypes(include='number')
+    corr_matrix = numeric_columns.corr()
+
+    # Identify parameters with correlations above the threshold
+    high_correlation_pairs = [(col1, col2, corr)
+                              for col1 in corr_matrix.columns
+                              for col2 in corr_matrix.columns
+                              if col1 < col2 and abs(corr := corr_matrix.loc[col1, col2]) > correlation_threshold]
+
+    # Create a set to store variables to drop
+    variables_to_drop = set()
+
+    for col1, col2, _ in high_correlation_pairs:
+        # Add the variable with a higher number of missing values to the set
+        if df[col1].isnull().sum() > df[col2].isnull().sum():
+            variables_to_drop.add(col2)
+        else:
+            variables_to_drop.add(col1)
+
+    # Drop the variables with high correlations and more missing values
+    df.drop(variables_to_drop, axis=1, inplace=True)
+    print(variables_to_drop)
+    return df
+
+
+def calculate_boxplots(df, has_outliers=True):
     for param in df.columns:
         if param != 'Timestamp':
             plt.figure(figsize=(20, 10))
-            sns.boxplot(x=df[param], showfliers=True)
-            plt.title(f'Boxplot for {param} with Outliers')
+            if has_outliers:
+                sns.boxplot(x=df[param], showfliers=True)
+            else:
+                sns.boxplot(x=df[param], showfliers=False)
+            if has_outliers:
+                plt.title(f'Boxplot for {param} with Outliers')
+            else:
+                plt.title(f'Boxplot for {param} with NO Outliers')
             plt.xlabel(param)
 
-            plt.savefig(os.path.join("./boxplots", f'boxplot_{param}.png'))
+            if has_outliers:
+                plt.savefig(os.path.join("./boxplots", f'boxplot_{param}.png'))
+            else:
+                plt.savefig(os.path.join("./boxplots", f'boxplot_{param}_no_outl.png'))
+
             plt.close()
 
-def calculate_correlation_matrix(df):
+
+def calculate_correlation_matrix(df, has_correlating_inputs=True):
     numeric_columns = df.select_dtypes(include='number')
 
     corr_matrix = numeric_columns.corr()
-
     output_dir = "./correlation_matrix"
     os.makedirs(output_dir, exist_ok=True)
+    if has_correlating_inputs:
 
-    plt.figure(figsize=(15, 10))
-    sns.heatmap(corr_matrix, annot=True, cmap='coolwarm', fmt=".2f", linewidths=.5,
-                cbar_kws={'label': 'Correlation Coefficient'})
-    plt.title('Correlation Matrix Heatmap')
+        plt.figure(figsize=(15, 10))
+        sns.heatmap(corr_matrix, annot=True, cmap='coolwarm', fmt=".2f", linewidths=.5,
+                    cbar_kws={'label': 'Correlation Coefficient'})
+        plt.title('Correlation Matrix Heatmap')
 
-    output_path = os.path.join(output_dir, 'correlation_heatmap.png')
-    plt.savefig(output_path)
-    plt.close()
+        output_path = os.path.join(output_dir, 'correlation_heatmap_with_correlating_inputs.png')
+        plt.savefig(output_path)
+        plt.close()
+    else:
+        plt.figure(figsize=(15, 10))
+        sns.heatmap(corr_matrix, annot=True, cmap='coolwarm', fmt=".2f", linewidths=.5,
+                    cbar_kws={'label': 'Correlation Coefficient'})
+        plt.title('Correlation Matrix Heatmap')
 
-    direct_correlations = corr_matrix.unstack().sort_values(ascending=False).drop_duplicates()
-    inverse_correlations = corr_matrix.unstack().sort_values(ascending=True).drop_duplicates()
+        output_path = os.path.join(output_dir, 'correlation_heatmap.png')
+        plt.savefig(output_path)
+        plt.close()
 
-    print("Top 5 Direct Correlations:")
-    print(direct_correlations.head(5))
-
-    print("\nTop 5 Inverse Correlations:")
-    print(inverse_correlations.head(5))
 
 if __name__ == '__main__':
-    file_path = "./SensorMLDataset_small.csv"
+    file_path = "./SensorMLDataset.csv"
     df = load_data(file_path)
-    df = preprocess_data(df)
+
+    df = preprocess_data_IQR(df)
+    calculate_boxplots(df, False)
     calculate_heatmaps(df)
-    calculate_boxplots(df)
-    calculate_correlation_matrix(df)
 
-
-
-
+    calculate_correlation_matrix(df, has_correlating_inputs=True)
+    df = feature_selection(df, correlation_threshold=0.90)
+    calculate_correlation_matrix(df, has_correlating_inputs=False)
